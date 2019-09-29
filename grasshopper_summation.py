639cb8a0af3d54034331ff3c9d42c8a03a09eff4
Elad Shamailov <eladsham@post.bgu.ac.il>
2019-09-29 20:09:13 +0300

    Update grasshopper_summation.py

--- grasshopper_summation.py
+++ grasshopper_summation.py
@@ -1,7 +1,7 @@
 """
 Write a program that finds the summation of every number from 1 to num. The number will always 
 be a positive integer greater than 0.
-""
+"""
 
 def summation(num):
     return num*(num+1)/2
